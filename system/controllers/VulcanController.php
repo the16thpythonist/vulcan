ef390caae4c78b360bd059227c6662ad535b2fad
jonse <jonseb1998@gmail.com>
2017-10-07 21:19:27 +0200

    Created new file VulcanController. VulcanController: dummy controller class, that extends the base controller and implements the controller interface.

---
+++ system/controllers/VulcanController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonse
+ * Date: 07.10.2017
+ * Time: 21:07
+ */
+
+require_once "ControllerBase.php";
+require_once "ControllerInterface.php";
+
+
+class VulcanController extends ControllerBase implements ControllerInterface
+{
+
+    public $viewClass;
+    public $view;
+
+    public function __construct($viewClass)
+    {
+        $this->viewClass = $viewClass;
+    }
+
+    public function createView()
+    {
+        $this->view = new $this->viewClass();
+    }
+
+    public function checkPost()
+    {
+        // TODO: Implement checkPost() method.
+    }
+
+    public function checkSession()
+    {
+        // TODO: Implement checkSession() method.
+    }
+
+    public function getViewString()
+    {
+        return $this->view->getString();
+    }
+
+}
