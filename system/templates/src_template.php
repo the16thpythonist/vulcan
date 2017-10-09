91234af08b3617cdc4a379354f75598ce0eb6220
jonse <jonseb1998@gmail.com>
2017-10-09 14:06:47 +0200

    Added the file 'src_template.php' from another project, which contains HTML string templates, that create the html linkings to scripts and stylesheets, when only given their filename

---
+++ system/templates/src_template.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonse
+ * Date: 28.09.2017
+ * Time: 16:58
+ */
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once $root . "/dChat/config.php";
+
+/**
+ * This template takes the names of the css stylesheets and the
+ * @param $stylesheetNames
+ * @return string
+ */
+function stylesheet_links($stylesheetNames)
+{
+    $tempStrings = "";
+    foreach ($stylesheetNames as $name) {
+        $path = URL_STYLES . $name;
+        // In case the name does not already contain the CSS file extension, adds it
+        if ( strpos($name, ".css") == false ){
+            $path .= ".css";
+        }
+        // Adding a linking to the stylesheet to the temp string
+        $tempStrings .= "<link href='$path' type='text/css' rel='stylesheet'>\r\n";
+    }
+
+    // Returning the complete string with all the stylesheet linkings
+    return $tempStrings;
+}
+
+function script_links($scriptNames, $async=TRUE){
+    $tempStrings = "";
+    foreach ($scriptNames as $name) {
+        $path = URL_SCRIPTS . $name;
+        // In case the name does not already contain the CSS file extension, adds it
+        if ( strpos($name, ".js") == false ){
+            $path .= ".js";
+        }
+        // Adding a linking to the stylesheet to the temp string
+        if ($async === TRUE) {
+            $tempStrings .= "<script src='$path' async></script>";
+        } else {
+            $tempStrings .= "<script src='$path'></script>";
+        }
+    }
+
+    // Returning the complete string with all the stylesheet linkings
+    return $tempStrings;
+}
